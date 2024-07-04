0a9b9cc714c6a17c92e600244fcc7497385a8c96
radwanrahman <162893024+radwanrahman@users.noreply.github.com>
2024-07-04 10:30:43 +0600

    Task-1.java

--- Task-1.java
+++ Task-1.java
@@ -1,3 +1,4 @@
+//Student class
 public class Student {
     private int id;
     private String name;
@@ -27,4 +28,38 @@ public class Student {
         System.out.println("Student Name: " + name);
     }
 
+}
+
+//Main Class
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        Student student2 = new Student(1);
+        Student student3 = new Student(2, "radwan");
+        Student student4 = new Student("kazi");
+        Main student5 = new Main();
+
+        System.out.println("Student 1:");
+        student1.display();
+        System.out.println();
+
+        System.out.println("Student 2:");
+        student2.display();
+        System.out.println();
+
+        System.out.println("Student 3:");
+        student3.display();
+
+        System.out.println("Student 4:");
+        student4.display();
+       
+        student5.print("Rayhan");
+        
+    }
+    public void print(String name){
+        System.out.println("Brother Name: "+ name);
+    }
+    
 }
